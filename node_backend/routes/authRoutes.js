c47ace0216a525dd1a6784005a7c15f3cdc2741c
Shreshtha-Garg <raghavgarg037@gmail.com>
2025-04-19 00:44:08 +0530

    minor changes

--- node_backend/routes/authRoutes.js
+++ node_backend/routes/authRoutes.js
@@ -10,39 +10,3 @@ router.post('/login', login);
 router.get('/profile', auth, getProfile);
 
 module.exports = router;
-
-// routes/exerciseRoutes.js
-const express = require('express');
-const { getExercises } = require('../controllers/exerciseController');
-const auth = require('../middleware/auth');
-
-const router = express.Router();
-
-router.get('/', auth, getExercises);
-
-module.exports = router;
-
-// routes/sessionRoutes.js
-const express = require('express');
-const { createSession, getSessions } = require('../controllers/sessionController');
-const auth = require('../middleware/auth');
-
-const router = express.Router();
-
-router.post('/', auth, createSession);
-router.get('/', auth, getSessions);
-
-module.exports = router;
-
-// routes/reportRoutes.js
-const express = require('express');
-const { getSpiderData } = require('../controllers/reportController');
-const auth = require('../middleware/auth');
-
-const router = express.Router();
-
-router.get('/spider', auth, getSpiderData);
-
-module.exports = router;
-
-
